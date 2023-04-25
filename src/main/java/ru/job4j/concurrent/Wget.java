655cc3e58cbeca50628eb02127b9f1c6e9db4518
Fredfromhell <Sl1pknot>
2023-04-25 15:24:02 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,23 +1,58 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import lombok.AllArgsConstructor;
+import org.apache.commons.validator.routines.IntegerValidator;
+import org.apache.commons.validator.routines.UrlValidator;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+@AllArgsConstructor
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    private final int MILLIS = 1000;
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[speed];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                long finish = System.currentTimeMillis();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long delta = finish - start;
+                if (delta < MILLIS) {
+                    Thread.sleep(MILLIS - delta);
+                }
+                start = System.currentTimeMillis();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
-        thread.join();
-        System.out.println("\rComplete");
+        UrlValidator urlValidator = new UrlValidator();
+        String url = args[0];
+        if (!urlValidator.isValid(url)) {
+            System.out.println("Некорректный URL в параметре 1");
+        }
+        try {
+            int speed = Integer.parseInt(args[1]);
+            Thread wget = new Thread(new Wget(url, speed));
+            wget.start();
+            wget.join();
+        } catch (NumberFormatException e) {
+            System.out.println("Введите скорость скачивания в параметр 2");
+        }
+
     }
 }
 
