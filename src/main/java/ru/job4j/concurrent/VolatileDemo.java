c6f51afb138e9f0569508b6869befe7c13971a69
Fredfromhell <Sl1pknot>
2023-07-05 09:30:54 +0300

    3.1. Добавить изменения в последний коммит. [#504829]

---
+++ src/main/java/ru/job4j/concurrent/VolatileDemo.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+public class VolatileDemo {
+
+    private static
+    int count = 0;
+
+    public static void main(String[] args) {
+
+        Runnable maker = () -> {
+            while (count < 5) {
+                System.out.println("Count inc from " + count + " to " + ++count);
+            }
+        };
+
+        Runnable listener = () -> {
+            int temp = 0;
+            while (temp < 5) {
+                if (temp != count) {
+                    System.out.println();
+                    System.out.println("Change detected! Now count = " + count);
+                    temp = count;
+                }
+            }
+        };
+
+       Thread one =  new Thread(listener);
+       Thread two = new Thread(maker);
+
+       two.start();
+       one.start();
+
+        System.out.println("Test");
+
+    }
+}
